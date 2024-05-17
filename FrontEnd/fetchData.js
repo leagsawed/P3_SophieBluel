bf311762cb4334b3e98025f64a10e8a4af546d73
Gael Dewas <gael.dewas@gmail.com>
2024-05-17 15:38:31 +0200

    création de la première figure et ajout dynamique de la figure à la gallerie

--- FrontEnd/fetchData.js
+++ FrontEnd/fetchData.js
@@ -1,6 +1,34 @@
 const url = 'http://localhost:5678/api/works';
 fetch(url)
   .then((response) => response.json())
-  .then((data) => console.log(data));
-
+  .then((data) => {
+    addProjects(data);
+  });
 const gallery = document.querySelector('.gallery');
+const figure = document.createElement('figure');
+
+function addProjects(data) {
+  console.log(data);
+  makeFigure(data);
+  appendFigureToGallery(figure);
+}
+
+function makeFigure(data) {
+  const img = document.createElement('img');
+  const figCaption = document.createElement('figcaption');
+  const title = data[0].title;
+  const imageUrl = data[0].imageUrl;
+
+  figure.appendChild(img);
+  img.src = imageUrl;
+  img.alt = title;
+  figure.appendChild(figCaption);
+  figCaption.innerHTML = title;
+
+  console.log(figure);
+}
+
+function appendFigureToGallery(figure) {
+  console.log(gallery);
+  gallery.appendChild(figure);
+}
